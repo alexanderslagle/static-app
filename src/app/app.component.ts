b44ec4b9bc5b0a29f643fba31667ce0edf414e90
Alex Slagle <alexander.slagle@emerson.com>
2025-04-09 11:29:30 -0500

    Added Azure Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   value = 'World';
+
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message')
+      .subscribe((resp: any) => this.message = resp.text);
+  }
 }
